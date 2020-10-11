118fa4da93184f380f5a2195e93e4415bf08f84f
Manix <admin@manix.info>
2020-10-11 20:28:18 +0300

    remove boiler from showcase

--- boiler/js/components/page.js
+++
@@ -1,9 +0,0 @@
-import Content from "../../../core/js/components/content.js";
-
-export default class Page extends Content {
-
-  titlex() {
-    return this.idx().replace("Page", "").replace(":", " - ");
-  }
-
-}
--- boiler/js/main.js
+++
@@ -1,4 +0,0 @@
-import Application from "./components/app.js";
-
-document.body.innerHTML = "";
-document.body.append(new Application);
